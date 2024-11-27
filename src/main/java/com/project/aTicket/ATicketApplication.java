3118a886cfca8c96091f7bc6035166b7fc0950e2
mquintero <mquinterosua@gmail.com>
2024-11-27 17:21:34 -0500

    initial commit

---
+++ src/main/java/com/project/aTicket/ATicketApplication.java
@@ -0,0 +1,13 @@
+package com.project.aTicket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ATicketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ATicketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/aTicket/ATicketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.aTicket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ATicketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
