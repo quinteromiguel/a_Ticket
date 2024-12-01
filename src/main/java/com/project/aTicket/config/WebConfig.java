e7300478e8862be5b6c4a7a3bd05e935367fd5d7
jorge1b3 <jorge1b3@hotmail.es>
2024-12-01 17:24:01 -0500

    ✨ feat: add cors and test

---
+++ src/main/java/com/project/aTicket/config/WebConfig.java
@@ -0,0 +1,16 @@
+package com.project.aTicket.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedOriginPatterns("http://127.0.0.1/*")
+                .allowedHeaders("*");
+    }
+}
